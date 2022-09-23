ba28f0714410f8f081e71d684570849d69054362
Gokul Gajapathi <akgokul22@gmail.com>
2022-09-23 23:07:37 +0530

    index of extra element

---
+++ src/com/gokul/IndexOfExtraElement.java
@@ -0,0 +1,46 @@
+package com.gokul;
+
+//Find an extra element present in arr1[]
+//GFG Link:https://practice.geeksforgeeks.org/problems/index-of-an-extra-element/1find%20an%20extra
+
+public class IndexOfExtraElement {
+    public static void main(String[] args) {
+        int arr1[] = {2, 3, 4, 6, 8,  12, 13};
+        int arr2[] = {2, 4, 6, 8, 10, 12};
+        int n = arr2.length;
+
+
+        System.out.println(findExtra(arr1, arr2, n));
+    }
+
+
+	static int findExtra(int arr1[], int arr2[], int n)
+	{
+
+		int index = n;
+
+
+		int left = 0, right = n - 1;
+		while (left <= right)
+		{
+			int mid = (left+right) / 2;
+
+
+			if (arr2[mid] == arr1[mid])
+				left = mid + 1;
+
+
+			else
+			{
+				index = mid;
+				right = mid - 1;
+			}
+		}
+
+
+		return index;
+	}
+
+
+
+}
